1692d6d48e170c00a6f9c480d5bcb60fb1fb8b41
Jean-Marc Coïc <jean-marc.coic@intersec.com>
2019-12-02 13:47:17 +0100

    Add script to init new days

---
+++ src/init-day.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DAY=$1
+
+if [ -z $DAY ]; then
+    echo "missind argument: day"
+    exit 1
+fi
+
+DIR="day$DAY"
+
+if [ -d $DIR ]; then
+    echo "day $DAY already exists"
+    exit 1
+fi
+
+mkdir $DIR
+
+echo "/day$DAY" > $DIR/.gitignore
+
+cat > $DIR/wscript_build <<XXX
+ctx.program(target='day$DAY', source='day$DAY.c',
+            use=['libcommon'])
+XXX
+
+echo "ctx.recurse('day$DAY')" >> wscript_build
